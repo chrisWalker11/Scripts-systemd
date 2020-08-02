00b365f6808652d33335f548b7e24be8dc0ab783
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-08-02 12:37:10 -0400

    Add files via upload

---
+++ virtualboxDownloader.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "enter version number"
+read a
+echo "read number after version number before Linux amd64"
+read b
+
+sudo pkill -9 virtualbox
+
+wget https://download.virtualbox.org/virtualbox/$a/VirtualBox-$a-$b-Linux_amd64.run
+
+chmod +x VirtualBox-$a-$b-Linux_amd64.run
+
+sudo ./VirtualBox-$a-$b-Linux_amd64.run
+
+rm VirtualBox-$a-$b-Linux_amd64.run
+
+sudo modprobe vboxguest vboxvideo vboxsf
+
+sudo systemctl enable --now vboxservice.service
---
+++ zoomUpdate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo pacman -R zoom
+sudo pacman -U /home/chris/Downloads/zoom_x86_64.pkg.tar.xz && rm /home/chris/Downloads/zoom_x86_64.pkg.tar.xz
