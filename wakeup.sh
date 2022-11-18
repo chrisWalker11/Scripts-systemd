649672e97531e227827cc0cf518b005d0919601b
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2022-11-18 14:33:30 -0500

    Update wakeup.sh

--- wakeup.sh
+++ wakeup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo rmmod i2c_hid
-sudo modprobe i2c_hid
+#i2c_hid said it was in use by i2c_hid_acpI changeing the scirpt to i2c_hid_acpi made the track pad work again
+#sudo rmmod i2c_hid
+#sudo modprobe i2c_hid
+sudo rmmod i2c_hid_acpi
+sudo modprobe i2c_hid_acpi
